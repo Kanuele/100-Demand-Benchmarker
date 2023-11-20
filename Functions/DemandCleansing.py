46f96ddd0daffc16b32eeb1b1ad6a51ad8d10723
Christian Birlo <christian@birlo.net>
2023-11-20 16:35:08 +0100

    Cleansing

--- Functions/DemandCleansing.py
+++ Functions/DemandCleansing.py
@@ -1,16 +1,19 @@
 import pandas as pd
 
-# Assuming your DataFrame is named 'df' and the column you want to group is named 'date'
-def date_to_month(df):
-    df['month'] = pd.DatetimeIndex(df['date']).month
-    df['year'] = pd.DatetimeIndex(df['date']).year
-    df['month_year'] = pd.to_datetime(df['date']).dt.to_period('M')
+# Group times series to month level
+def aggregate_to_month(df):
+    df['Date'] = pd.to_datetime(df['Date'])
+    #df.set_index('Date', inplace=True)
+    header = df.columns.values
+    header = list(header[header != "demand_quantity"])
+    df = df.groupby(header).agg({"demand_quantity": "sum"})#.reset_index()
     return df
 
-# Group times series to month level
+demand_grouped = aggregate_to_month(demand)
 
-# set negative values to 0
+demand_grouped["demand_quantity"].fillna(0, inplace=True) # Fill up missing values with 0
+demand_grouped["demand_quantity"] = demand_grouped["demand_quantity"].clip(lower=0) # set negative values to 0
 
-# Fill up missing values with 0
+# check if per time series all month are present
 
 # Remove outliers
