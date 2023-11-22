eb939b23de035f37e82f0cf7b4ce769f5e287479
Christian Birlo <christian@birlo.net>
2023-11-22 07:01:22 +0100

    enhanced aggregate function

--- Functions/DemandCleansing.py
+++ Functions/DemandCleansing.py
@@ -9,7 +9,7 @@ def aggregate_to_month(df):
     df = df.groupby(header).agg({"demand_quantity": "sum"})#.reset_index()
     return df
 
-def aggregate_all_attributes(df):
+def aggregate_all_attributes(df): #hiervon ausgehend Darstellung der Gruppen in Pareto und selection der verwendeten attribute
     df['Date'] = pd.to_datetime(df['Date'])
     #df.set_index('Date', inplace=True)
     header = df.columns.values
@@ -18,7 +18,26 @@ def aggregate_all_attributes(df):
     df = df.groupby(header).agg({"demand_quantity": "sum"})#.reset_index()
     return df
 
-demand_grouped = aggregate_to_month(demand)
+def aggregate(df): #erweitern durch Auswahl der Attribute, die aggregiert werden sollen
+    df['Date'] = pd.to_datetime(df['Date'])
+    header = df.columns.values
+    header = list(header[header != "demand_quantity"])
+    header.remove("Date")
+    header.append("Date")
+    df = (df.groupby([pd.Grouper(key="Date", freq="M"), 'Store_city', 'Store_Country', 'Product Name', 'Product Group', 'Product Family'])
+        .sum()
+        .reset_index("Date")
+        .sort_index())
+    return df
+
+'''
+
+demand_grouped = aggregate(demand)
+
+demand_filtered = demand_grouped.xs(("Zapopan", "Mexico", "Fancy Beer Ale 1 pint"))#.reset_index()#.set_index("Date") # select one time series
+
+# check that all months from start to previous months are available
+
 
 demand_grouped["demand_quantity"].fillna(0, inplace=True) # Fill up missing values with 0
 demand_grouped["demand_quantity"] = demand_grouped["demand_quantity"].clip(lower=0) # set negative values to 0
@@ -28,13 +47,4 @@ demand_all_attr = aggregate_all_attributes(demand)
 # check if per time series all month are present
 
 # Remove outliers
-
-
-import pandas as pd
-from darts import TimeSeries
-
-# Create a TimeSeries, specifying the time and value columns
-series = TimeSeries.from_dataframe(demand, "Date", "demand_quantity")
-
-# Set aside the last 36 months as a validation series
-train, val = series[:-36], series[-36:]
+'''
--- main.py
+++ main.py
@@ -4,8 +4,20 @@ import pandas as pd
 import pyarrow as pa #pyarrow to use parquet files
 import pyarrow.parquet as pq
 
+import statsmodels.api as sm
+
 import Functions.ImportFunctions as import_functions
+import Functions.ExportResults as export_functions
+from Functions.DemandCleansing import aggregate as dc_aggregate
+# import Functions.DemandCleansing as demand_cleansing
+
 
 import_file = import_functions.import_file
 
 demand  = import_file('ExampleData/Forecasting_beer.parquet')
+
+aggregated_df = dc_aggregate(demand)
+demand_filtered = aggregated_df.xs(("Zapopan", "Mexico", "Fancy Beer Ale 1 pint"))#.reset_index()
+
+# ----- Export
+demand_filtered.to_csv("ExampleData/Forecasting_beer_filtered.csv")
