b9e17c26c0e6f777ecf25d411009cdaa8a5361e1
Christian Birlo <christian@birlo.net>
2023-11-23 17:39:24 +0100

    Added fill with missing periods

--- Functions/DemandCleansing.py
+++ Functions/DemandCleansing.py
@@ -1,22 +1,23 @@
 import pandas as pd
+import numpy as np
 
 # Group times series to month level
-def aggregate_to_month(df):
-    df['Date'] = pd.to_datetime(df['Date'])
-    #df.set_index('Date', inplace=True)
-    header = df.columns.values
-    header = list(header[header != "demand_quantity"])
-    df = df.groupby(header).agg({"demand_quantity": "sum"})#.reset_index()
-    return df
+# def aggregate_to_month(df):
+#     df['Date'] = pd.to_datetime(df['Date'])
+#     #df.set_index('Date', inplace=True)
+#     header = df.columns.values
+#     header = list(header[header != "demand_quantity"])
+#     df = df.groupby(header).agg({"demand_quantity": "sum"})#.reset_index()
+#     return df
 
-def aggregate_all_attributes(df): #hiervon ausgehend Darstellung der Gruppen in Pareto und selection der verwendeten attribute
-    df['Date'] = pd.to_datetime(df['Date'])
-    #df.set_index('Date', inplace=True)
-    header = df.columns.values
-    header = list(header[header != ["demand_quantity"]])
-    header.remove("Date")
-    df = df.groupby(header).agg({"demand_quantity": "sum"})#.reset_index()
-    return df
+# def aggregate_all_attributes(df): #hiervon ausgehend Darstellung der Gruppen in Pareto und selection der verwendeten attribute
+#     df['Date'] = pd.to_datetime(df['Date'])
+#     #df.set_index('Date', inplace=True)
+#     header = df.columns.values
+#     header = list(header[header != ["demand_quantity"]])
+#     header.remove("Date")
+#     df = df.groupby(header).agg({"demand_quantity": "sum"})#.reset_index()
+#     return df
 
 def aggregate(df): #erweitern durch Auswahl der Attribute, die aggregiert werden sollen
     df['Date'] = pd.to_datetime(df['Date'])
@@ -30,21 +31,53 @@ def aggregate(df): #erweitern durch Auswahl der Attribute, die aggregiert werden
         .sort_index())
     return df
 
-'''
+# Combine all attributes
+def combine_attributes(df, date_name = "Date", key_figures = "demand_quantity"):
+    col_names = set(list(df.columns))
+    values = set([date_name, key_figures])
+    col_names = list(col_names - values)
 
-demand_grouped = aggregate(demand)
+    df["combined"] = pd.Series(df[col_names].values.tolist()).str.join(' // ')
+    df = df.drop(col_names, axis=1)
+    return df
 
-demand_filtered = demand_grouped.xs(("Zapopan", "Mexico", "Fancy Beer Ale 1 pint"))#.reset_index()#.set_index("Date") # select one time series
+# fills missing periods with 0 for a series --> need to iterate over all time series
+def fill_with_zeros(df):
+    df.asfreq('M', fill_value=0.0)
+    df.reset_index(inplace=True)
+    return df
 
-# check that all months from start to previous months are available
+# # Fill missing periods
+def iterate_combinations(df):
+    combinations = df["combined"].unique()
+    new_df = pd.DataFrame()
+
+    for combination in combinations:
+        subset = df.loc[df["combined"] == combination]
+        subset = fill_with_zeros(subset)
+        new_df = pd.concat([new_df, subset])
+
+    return new_df
 
 
-demand_grouped["demand_quantity"].fillna(0, inplace=True) # Fill up missing values with 0
-demand_grouped["demand_quantity"] = demand_grouped["demand_quantity"].clip(lower=0) # set negative values to 0
+def split_column(df, column_name, delimiter, col_names):
+    new_columns = df[column_name].str.split(delimiter, expand=True)
+    new_column_names = col_names[:new_columns.shape[1]]
+    df[new_column_names] = new_columns
+    df.drop(column_name, axis=1, inplace=True)
+    return df
 
-demand_all_attr = aggregate_all_attributes(demand)
 
-# check if per time series all month are present
 
-# Remove outliers
-'''
+# # check that all months from start to previous months are available
+
+
+# demand_grouped["demand_quantity"].fillna(0, inplace=True) # Fill up missing values with 0
+# demand_grouped["demand_quantity"] = demand_grouped["demand_quantity"].clip(lower=0) # set negative values to 0
+
+# demand_all_attr = aggregate_all_attributes(demand)
+
+# # check if per time series all month are present
+
+# # Remove outliers
+# '''
--- main.py
+++ main.py
@@ -4,22 +4,46 @@ import pandas as pd
 import pyarrow as pa #pyarrow to use parquet files
 import pyarrow.parquet as pq
 
-import statsmodels.api as sm
+# import statsmodels.api as sm
 
 import Functions.ImportFunctions as import_functions
 import Functions.ExportResults as export_functions
-from Functions.DemandCleansing import aggregate as dc_aggregate
+import Functions.DemandCleansing as dc
+# from Functions.DemandCleansing import aggregate as dc_aggregate
 import Functions.ForecastAlgorithms as FFA
 # import Functions.DemandCleansing as demand_cleansing
 
+# Import
 
 import_file = import_functions.import_file
-
 demand  = import_file('ExampleData/Forecasting_beer.parquet')
 
-aggregated_df = dc_aggregate(demand)
-demand_filtered = aggregated_df.xs(("Zapopan", "Mexico", "Fancy Beer Ale 1 pint"))#.reset_index()
+# Take Colnames
+
+col_names = set(list(demand.columns))
+values = set(["Date", "demand_quantity"])
+col_names = list(col_names - values)
+
+
+# Cleanse
+
+## Aggregate to month level
+aggregated_df = dc.aggregate(demand)
+
+demand = aggregated_df.reset_index()
+
+## Fill missing periods
+
+demand = dc.combine_attributes(demand)
+demand.set_index(["Date"], inplace=True)
+demand = dc.iterate_combinations(demand)
+demand = dc.split_column(demand, 'combined', ' // ', col_names)
+
+
+
 
 forecast = FFA.moving_average(demand_filtered["demand_quantity"], extra_periods=12, n=3) # apend the results to the dataframe
 # ----- Export
 demand_filtered.to_csv("ExampleData/Forecasting_beer_filtered.csv")
+
+demand_sub.to_csv("ExampleData/Forecasting_beer_sub.csv")
