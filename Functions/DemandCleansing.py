1f2196e71c49839e764491238224ef21c652cfa9
Christian Birlo <christian@birlo.net>
2023-11-24 16:05:22 +0100

    Outlier Correction added

--- Functions/DemandCleansing.py
+++ Functions/DemandCleansing.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import norm
 
 # Group times series to month level
 # def aggregate_to_month(df):
@@ -80,4 +81,45 @@ def split_column(df, column_name, delimiter, col_names):
 # # check if per time series all month are present
 
 # # Remove outliers
-# '''
+# Winsorization
+# Corrects also lower data points, which are not outliers
+def outlier_winsorization(df, demand_col, lower_percentile = 1, upper_percentile = 99):
+    limit_high = np.percentile(df[demand_col], upper_percentile)
+    limit_low = np.percentile(df[demand_col], lower_percentile)
+    
+    df["demand_corrected"] = np.where(df[demand_col] < limit_low, limit_low, df[demand_col])
+    df["demand_corrected"] = np.where(df[demand_col] > limit_high, limit_high, df[demand_col])
+    return df
+
+# Standard deviation
+# Corrects also data points, which are not outliers if pattern follows seasonality or trend
+
+## 1. Identify very high errogenous values
+
+def outlier_stddev(df, demand_col, std_dev = 3):
+    mean = df[demand_col].mean()
+    std = df[demand_col].std()
+    df["demand_corrected"] = np.where(df[demand_col] > mean + std_dev * std, mean + std_dev * std, df[demand_col])
+    df["demand_corrected"] = np.where(df[demand_col] < mean - std_dev * std, mean - std_dev * std, df[demand_col])
+    return df
+
+def outlier_stdnorm(df, demand_col, lower_percentile = 0.01, upper_percentile = 0.99):
+    mean = df[demand_col].mean()
+    std = df[demand_col].std()
+
+    limit_high = norm.ppf(upper_percentile, mean, std)
+    limit_low = norm.ppf(lower_percentile, mean, std)
+    df["demand_corrected"] = np.where(df[demand_col] > limit_high, limit_high, df[demand_col])
+    df["demand_corrected"] = np.where(df[demand_col] < limit_low, limit_low, df[demand_col])
+    return 
+
+# Stndard deviation of error
+def outlier_error_stddev(df, demand_col, forecast_col, lower_percentile = 0.01, upper_percentile = 0.99):
+    df["error"] = df[demand_col] - df[forecast_col]
+    mean = df["error"].mean()
+    std = df["error"].std()
+    limit_high = norm.ppf(upper_percentile, mean, std) + df[forecast_col]
+    limit_low = norm.ppf(lower_percentile, mean, std) + df[forecast_col]
+    df["demand_corrected"] = np.where(df[demand_col] > limit_high, limit_high, df[demand_col])
+    df["demand_corrected"] = np.where(df[demand_col] < limit_low, limit_low, df[demand_col])
+    return df
